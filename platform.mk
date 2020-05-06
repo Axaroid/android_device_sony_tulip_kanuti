63ee8243c870878422d71c1a1ea23f9cd40e2e33
Axaroid <aksharndrds@gmail.com>
2020-05-06 16:09:02 +0530

    Added some files and edited some files.

---
+++ platform.mk
@@ -0,0 +1,119 @@
+# Copyright (C) 2014 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Platform path
+PLATFORM_COMMON_PATH := device/sony/tulip
+
+TARGET_LEGACY_KEYMASTER := true
+
+$(call inherit-product, device/sony/common/common.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
+SOMC_PLATFORM := kanuti
+SOMC_KERNEL_VERSION := 4.4
+
+SONY_ROOT := $(PLATFORM_COMMON_PATH)/rootdir
+
+# Qualcomm WiFi
+PRODUCT_COPY_FILES += \
+    $(SONY_ROOT)/vendor/etc/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
+    $(SONY_ROOT)/vendor/etc/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf
+
+# Keylayout
+PRODUCT_COPY_FILES += \
+    $(SONY_ROOT)/vendor/usr/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gpio-keys.kl
+
+# IDC
+PRODUCT_COPY_FILES += \
+    $(SONY_ROOT)/vendor/usr/idc/cyttsp4_mt.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/cyttsp4_mt.idc \
+    $(SONY_ROOT)/vendor/usr/idc/cyttsp5_mt.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/cyttsp5_mt.idc
+
+# MSM IRQ Balancer configuration file
+PRODUCT_COPY_FILES += \
+    $(SONY_ROOT)/vendor/etc/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
+
+# RQBalance-PowerHAL configuration
+PRODUCT_COPY_FILES += \
+    $(SONY_ROOT)/vendor/etc/rqbalance_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/rqbalance_config.xml
+
+# Platform specific init
+PRODUCT_PACKAGES += \
+    init.kanuti \
+    init.kanuti.pwr \
+    ueventd
+
+# GFX
+PRODUCT_PACKAGES += \
+    gralloc.msm8916 \
+    copybit.msm8916 \
+    hwcomposer.msm8916 \
+    memtrack.msm8916
+
+# GPS
+PRODUCT_PACKAGES += \
+    gps.msm8916
+
+# CAMERA
+PRODUCT_PACKAGES += \
+    camera.msm8916
+
+# Bluetooth
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.qualcomm.bt.hci_transport=smd
+
+# aDSP sensors
+## max rate
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.qti.sensors.max_accel_rate=false \
+    ro.qti.sensors.max_gyro_rate=false \
+    ro.qti.sensors.max_mag_rate=false \
+    ro.qti.sensors.max_geomag_rotv=50
+
+## sensor type
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.qti.sdk.sensors.gestures=false \
+    ro.qti.sensors.pedometer=false \
+    ro.qti.sensors.step_detector=false \
+    ro.qti.sensors.step_counter=false \
+    ro.qti.sensors.pam=false \
+    ro.qti.sensors.scrn_ortn=false \
+    ro.qti.sensors.smd=true \
+    ro.qti.sensors.game_rv=false \
+    ro.qti.sensors.georv=false \
+    ro.qti.sensors.cmc=false \
+    ro.qti.sensors.bte=false \
+    ro.qti.sensors.fns=false \
+    ro.qti.sensors.qmd=false \
+    ro.qti.sensors.amd=false \
+    ro.qti.sensors.rmd=false \
+    ro.qti.sensors.vmd=false \
+    ro.qti.sensors.gtap=false \
+    ro.qti.sensors.tap=false \
+    ro.qti.sensors.facing=false \
+    ro.qti.sensors.tilt=false \
+    ro.qti.sensors.tilt_detector=false \
+    ro.qti.sensors.dpc=false \
+    ro.qti.sensors.wu=false
+
+# USB controller setup
+PRODUCT_PROPERTY_OVERRIDES += \
+    sys.usb.controller=msm_hsusb
+
+# WiFi MAC address path
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.wifi.addr_path=/sys/devices/soc/a000000.qcom,wcnss-wlan/wcnss_mac_addr
+
+# setup dm-verity configs.
+PRODUCT_SYSTEM_VERITY_PARTITION := /dev/block/platform/soc/7824900.sdhci/by-name/system
+$(call inherit-product, build/target/product/verity.mk)
